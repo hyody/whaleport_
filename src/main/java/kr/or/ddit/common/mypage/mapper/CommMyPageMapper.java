8c8d5106a688ed2e439ac8af123f0ea60ad6d879
PC-21 <PC-21@DESKTOP-SGDE0PP>
2024-12-02 14:46:25 +0900

    whaleport

--- src/main/java/kr/or/ddit/common/mypage/mapper/CommMyPageMapper.java
+++ src/main/java/kr/or/ddit/common/mypage/mapper/CommMyPageMapper.java
@@ -4,7 +4,7 @@ import kr.or.ddit.common.login.vo.LoginCommonVO;
 
 public interface CommMyPageMapper {
 	
-	// 직원 정보 조회
+	// 직원 정보 조회!
 	public LoginCommonVO selectProfile(LoginCommonVO loginCommonVO);
 	
 	// 직원 정보 수정
